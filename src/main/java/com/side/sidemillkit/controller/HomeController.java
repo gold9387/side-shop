5a9cec522c5e7fe679ee541c84cac7093772de61
gold9387 <gold9387@gmail.com>
2023-05-29 21:59:29 +0900

    create login join update UserController build.gradle

--- src/main/java/com/side/sidemillkit/controller/HomeController.java
+++ src/main/java/com/side/sidemillkit/controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.side.sidemillkit.controller;
 
-import com.side.sidemillkit.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
 
-    private final UserService userService;
-
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String dashboard() {
+        return "dashboard";
     }
 }
