d0c75d5cfc118d2a6d3b894484ad37660846cc05
gold9387 <gold9387@gmail.com>
2023-05-29 16:23:00 +0900

    new project

---
+++ src/main/java/com/side/sidemillkit/SideMillkitApplication.java
@@ -0,0 +1,13 @@
+package com.side.sidemillkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideMillkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideMillkitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/side/sidemillkit/SideMillkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.sidemillkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideMillkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
