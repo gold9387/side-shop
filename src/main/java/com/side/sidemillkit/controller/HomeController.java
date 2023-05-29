eb76897f14d4b733220a3227fe86bc9b723aba62
gold9387 <gold9387@gmail.com>
2023-05-29 19:05:22 +0900

    create usercontroller userdto userservice join.html

--- src/main/java/com/side/sidemillkit/controller/HomeController.java
+++ src/main/java/com/side/sidemillkit/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.side.sidemillkit.controller;
 
+import com.side.sidemillkit.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserService userService;
 
     @GetMapping("/")
     public String index() {
