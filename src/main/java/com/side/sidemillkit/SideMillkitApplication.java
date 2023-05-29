fdc52093a60d711f8863839b22b6e4acca33ded5
gold9387 <gold9387@gmail.com>
2023-05-29 16:25:44 +0900

    new commite

--- src/main/java/com/side/sidemillkit/SideMillkitApplication.java
+++ src/main/java/com/side/sidemillkit/SideMillkitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SideMillkitApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SideMillkitApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SideMillkitApplication.class, args);
+	}
 
 }
