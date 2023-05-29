772f36625afe0d79efb7e9c32d2f7790513113d4
gold9387 <gold9387@gmail.com>
2023-05-29 17:42:49 +0900

    properties setup entity repository create

---
+++ src/main/java/com/side/sidemillkit/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.side.sidemillkit.repository;
+
+import com.side.sidemillkit.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
